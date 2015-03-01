c2ca9bb11ed2e98761d4ea33748f4f6478c0315a
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-03-01 13:03:00 +0100

    Created first classes

---
+++ src/main/com/wonnova/sdk/auth/Credentials.java
@@ -0,0 +1,12 @@
+package com.wonnova.sdk.auth;
+
+/**
+ *
+ */
+public interface Credentials {
+
+    /**
+     * @return String
+     */
+    public String getKey();
+}
---
+++ src/test/com/wonnova/sdk/connection/ClientTest.java
@@ -0,0 +1,20 @@
+package com.wonnova.sdk.connection;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by alejandro on 1/03/15.
+ */
+public class ClientTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testOne() {
+
+    }
+}
